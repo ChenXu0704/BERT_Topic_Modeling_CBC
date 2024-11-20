feaaa3fa2874e0030ebc50b2eee680fab8462796
ChenXu0704 <chenxu0704go@gmail.com>
2024-11-20 15:06:53 -0500

    Output the screened articles as csv file

--- news_screen.py
+++ news_screen.py
@@ -1,7 +1,7 @@
-from ScrapingNews import ScrapeNews
 from bs4 import BeautifulSoup
 from omegaconf import OmegaConf
 import requests
+import pandas as pd
 def news_screening(config):
   headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
   title_element = config.screen.title_element
@@ -11,25 +11,31 @@ def news_screening(config):
   match_patterns = config.screen.match_pattern
   infile = config.scrape.output.search_path
   outputfile = config.screen.output.screen_path
-  with open(outputfile, "w") as outfile:
-    f = open(infile, "r")
-    for line in f:
-      try:
-        if len(line.replace('\n', '')) == 0:
-            continue
-        line = line.replace('\n', '')
-        soup = BeautifulSoup(requests.get(line, headers=headers).text, 'html.parser')
-        print(line)
-        head = soup.find(title_element).get_text().replace('\n',' ').lower()
-        body = soup.find(article_element, class_=article_element_name).get_text().replace('\n',' ').lower()
-        content = head + ' ' + body
-        for pattern in match_patterns:
-          if pattern in content:
-            outfile.write(f"{line}\n")
-            break
-      except:
-        continue
-                    
+  if outputfile.split('.')[-1] != 'csv':
+    print("output should be in csv format.")
+    exit()
+  res = []
+  f = open(infile, "r")
+  index = 0
+  for line in f:
+    try:
+      if len(line.replace('\n', '')) == 0:
+          continue
+      line = line.replace('\n', '')
+      soup = BeautifulSoup(requests.get(line, headers=headers).text, 'html.parser')
+      print(line)
+      head = soup.find(title_element).get_text().replace('\n',' ').lower()
+      body = soup.find(article_element, class_=article_element_name).get_text().replace('\n',' ').lower()
+      content = head + ' ' + body
+      for pattern in match_patterns:
+        if pattern in content:
+          res.append([index, content])
+          index += 1
+          break
+    except:
+      continue
+  df = pd.DataFrame(res, columns=['id','text'])
+  df.to_csv(outputfile, index=False)               
 
 
 if __name__ == '__main__':
