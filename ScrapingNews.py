19e2547da037c48350187c06a09478de2f809674
ChenXu0704 <chenxu0704go@gmail.com>
2024-11-20 14:51:25 -0500

    Modify the output as csv for next step

--- ScrapingNews.py
+++ ScrapingNews.py
@@ -1,8 +1,9 @@
-import time, re, requests
+import time, requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from omegaconf import OmegaConf
+import pandas as pd
 
 class ScrapeNews():
     def __init__(self):
@@ -40,7 +41,7 @@ class ScrapeNews():
                 if item['data-cy'] == 'type-story':
                     link = item['href']
                     self.news_urls.add(f"https://www.cbc.ca{link}")
-        self.output(self.output_file)
+        self.output_as_csv(self.output_file)
     
     def lazy_scroll(self, needPressButton = True):
         current_height = self.driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
@@ -60,12 +61,19 @@ class ScrapeNews():
                 return self.driver.page_source
             current_height = new_height
             
-    def output(self, output_file):
-        open(output_file, 'a').close()
-        with open(output_file, 'w') as file:
-            for item in self.news_urls:
-                file.write(item)
-                file.write('\n')
+    def output_as_csv(self, output_file):
+        if output_file.split('.')[-1] != 'csv':
+            print("Output should be csv format.")
+            exit()
+        headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+        res = []
+        for i in range(self.news_urls):
+          url = self.news_ulrs[i]
+          parser = BeautifulSoup(requests.get(url, headers=headers).text, 'html.parser')
+          text = parser.find('h1').get_text() + ' ' + parser.find('div', class_='story').get_text()
+          res.append([i, text])
+        df = pd.DataFrame(res, columns=['id','text'])
+        df.to_csv(index=False)
 
 if __name__ == '__main__':
   config = OmegaConf.load('./params.yaml')
